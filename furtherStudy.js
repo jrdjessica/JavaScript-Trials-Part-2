d73fc686f4f7c21ab3a7a45008b3af568136067a
Jessica Dea <jessicardea@gmail.com>
2022-09-04 22:45:31 -0700

    Added functions

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,8 +1,24 @@
 'use strict';
 
+
+
+
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const result = new Set();
+
+  const words1Set = new Set(words1)
+  const words2Set = new Set(words2)
+
+  for (const word of words1Set) {
+    result.add(word);
+  }
+  for (const word of words2Set) {
+    result.add(word);
+  }
+  return Array.from(result)
 }
+
+// console.log(wordsInCommon(['hello', 'hi', 'hey'], ['hello', 'yo', 'hi']))
 
 function kidsGame(names) {
   // Replace this with your code
--- objects.js
+++ objects.js
@@ -2,10 +2,35 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {};
+
+  for (const word of phrase.split(' ')) {
+    if (wordCounts.hasOwnProperty(word)) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
+  }
+  return wordCounts
 }
+
+// console.log(countWords('before you start open up another browser before you start before'))
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melon_prices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  }
+
+  if (melon_prices.hasOwnProperty(price)) {
+    return melon_prices[price];
+  }
+  else {
+    return
+  }
 }
+
+// console.log(getMelonsAtPrice(2.50))
